ce15560baa8f8353dada68c900082cd9845edd8f
test <omerfaruk38baran@gmail.com>
2024-01-02 11:38:34 +0300

    Array list içerisinde en küçük yakın sayıyı ve en büyük yakın sayıyı ekrana yazdırır.

--- ArrayAverage/src/Main.java
+++ ArrayAverage/src/Main.java
@@ -1,24 +1,15 @@
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Lütfen Bir Sayı Giriniz : ");
+        int girdi = scan.nextInt();
+            int[] list = {15,12,788,1,-1,-778,2,0};
+            for (int i : list){
+                if ()
+            }
 
-        Scanner getData = new Scanner(System.in);
-
-        System.out.print("Lütfen Bir Sayı Giriniz : ");
-
-        int number = getData.nextInt();
-
-        double harmonicSum = 0.0;
-
-        for (int i = 1; i <= number; i++) {
-
-            harmonicSum += 1.0 / i;
-
-        }
-
-        System.out.println(number + " sayısının harmonik serisi : " + harmonicSum);
     }
 
 }
