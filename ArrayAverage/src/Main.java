c09948b0e9e6c9ea2141ee695c16aac7203eabb5
Ömer Faruk BARAN <omerfaruk38baran@gmail.com>
2024-01-02 14:57:05 +0300

    Update Main.java

--- ArrayAverage/src/Main.java
+++ ArrayAverage/src/Main.java
@@ -1,15 +1,1 @@
-import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Lütfen Bir Sayı Giriniz : ");
-        int girdi = scan.nextInt();
-            int[] list = {15,12,788,1,-1,-778,2,0};
-            for (int i : list){
-                if ()
-            }
-
-    }
-
-}
